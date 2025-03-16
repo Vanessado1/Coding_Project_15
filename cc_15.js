c8e3759d2bb236a9aceb20cc0a5a4db9c6e97d12
Vanessa Do <xex.vanessa@gmail.com>
2025-03-16 00:26:33 -0400

    Task 1- Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,5 @@
+// Task 1 
+document.addEventListener('DOMContentLoaded', (event) =>{
+    const riskDashboard = document.getElementById('riskDashboard');
+    console.log("Risk Dashboard Loaded");
+});
